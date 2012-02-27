7b7b5b335a1cd1b473de602562b658237e04b119
Daniel Norris <dnorris10@gmail.com>
2012-02-27 10:22:56 -0800

    Adds send method to class in tests

--- spec/cotsserver_spec.rb
+++ spec/cotsserver_spec.rb
@@ -7,12 +7,15 @@ class SampleClient < EventMachine::Connection
     Sample = '{"id":"0.0.0.0","timestamp":"2012-02-08 06:01:43.788"}'
     def initialize
         @data = []
-        send_data Sample
     end
 
     def receive_data(data)
         @data << data
         @ondata.call if @ondata
+    end
+    
+    def send
+        send_data Sample
     end
 
     def unbind
@@ -25,6 +28,7 @@ describe COTServer do
     server = COTServer.new(configuration)
     host = '0.0.0.0'
     port = server.directives[:port]
+
     it "validates basic JSON" do 
         EM.run do
             EventMachine::start_server host, port, JSONResponder
@@ -34,6 +38,7 @@ describe COTServer do
                 socket.data.last.chomp.should == JSON.generate(JSONResponder::Success)
                 EM.stop
             }
+            socket.send
         end
     end
 end
